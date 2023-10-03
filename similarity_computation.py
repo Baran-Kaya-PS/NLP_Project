c75bb9cf96b43767066cd718541c4255b33561d6
Théo <89530648+SpottyShadow@users.noreply.github.com>
2023-10-03 16:12:15 +0200

    Update similarity_computation.py

--- similarity_computation.py
+++ similarity_computation.py
@@ -1,21 +1,26 @@
 import numpy as np
+import os
 from sklearn.metrics.pairwise import cosine_similarity
-from embedding import model, read_sentences_from_files
+from embedding import model, read_sentences_from_files, obtain_embeddings
 
 def compute_similarity(vector1, vector2):
     """Compute cosine similarity between two vectors."""
     return cosine_similarity([vector1], [vector2])[0][0]
 
-def find_most_similar_sentences(query_sentence, embeddings, sentences, top_n=5):
+def find_most_similar_sentences(query_sentence, loaded_embeddings, sentences, top_n=5):
     """Find the top_n most similar sentences to the query_sentence."""
     # Obtain the embedding for the query sentence
-    query_embedding = model.encode([query_sentence])[0]
+    query_embeddings = obtain_embeddings(os.path.join(os.getcwd(), 'sous-titres'))
 
-    # Compute similarities with all sentences in the dataset
-    similarities = [compute_similarity(query_embedding, emb) for emb in embeddings]
+    # Compute the cosine similarity between the query embedding and all sentence embeddings
+    similarity_scores = np.dot(loaded_embeddings, query_embeddings.T) / (np.linalg.norm(loaded_embeddings, axis=1) * np.linalg.norm(query_embeddings, axis=1))
 
-    # Get the indices of the top_n most similar sentences
-    top_indices = np.argsort(similarities)[-top_n:][::-1]
+    # Get the indices of the top N most similar sentences
+    top_indices = np.argsort(similarity_scores, axis=0)[::-1][:top_n]
 
-    # Return the top_n most similar sentences along with their similarity scores
-    return [(sentences[i], similarities[i]) for i in top_indices]
+    # Get the file names, sentences, and similarity scores for the top N most similar sentences for each query sentence
+    top_similar_sentences = []
+    for i in range(len(query_sentence)):
+        top_similar_sentences.append([(sentences[j][0], sentences[j][1], similarity_scores[j][i]) for j in top_indices[:, i]])
+
+    return top_similar_sentences
