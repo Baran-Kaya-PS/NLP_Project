9fff9d58be112bd3fae6a380d9df08c129d180cd
Théo <89530648+SpottyShadow@users.noreply.github.com>
2023-10-24 11:57:28 +0200

    Update generate_json.py
    Changement json

--- generate_json.py
+++ generate_json.py
@@ -20,6 +20,31 @@ r'(S\d{1,2}E\d{1,2})',
     r'(s\d{2}ep\d{2})'
 ]
 
+# Liste des séries
+nomSerie = ["24", "90210", "alias", "angel", "battlestargalactica", "betteroffted", "bionicwoman", "blade", "bloodties",
+            "bones", "breakingbad", "buffy", "burnnotice", "californication", "caprica", "charmed", "chuck", "coldcase",
+            "community", "criminalminds", "cupid", "daybreak", "demons", "desperatehousewives", "dexter", "dirt",
+            "dirtysexymoney", "doctorwho", "dollhouse", "eleventhhour", "entourage", "eureka", "extras", "fearitself",
+            "flashforward", "flashpoint", "flightoftheconchords", "fridaynightlights", "friends", "fringe", "futurama",
+            "garyunmarried", "ghostwhisperer", "gossipgirl", "greek", "greysanatomy", "heroes", "house",
+            "howimetyourmother", "intreatment", "Invasion", "jake", "jekyll", "jericho", "johnfromcincinnati",
+            "Journeyman", "knightrider", "kylexy", "legendoftheseeker", "leverage", "lietome", "life", "lost",
+            "madmen", "mastersofscifi", "medium", "melroseplace", "mental", "merlin", "moonlight", "mynameisearl",
+            "nas", "ncislosangeles", "niptuck", "onetreehill", "painkillerjane", "primeval", "prisonbreak",
+            "privatepractice", "psych", "pushingdaisies", "raines", "reaper", "robinhood", "rome", "samanthawho",
+            "sanctuary", "scrubs", "sexandthecity", "sixfeetunder", "skins", "smallville", "sonsofanarchy", "southpark",
+            "spaced", "stargateatlantis", "stargatesgl", "stargateuniverse", "supernatural", "swingtown", "the4400",
+            "thebigbangtheory", "theblackdonnellys", "thekillpoint", "thelostroom", "thementalist", "thenine", "theoc",
+            "thepretender", "theriches", "thesarahconnorchronicles", "theshield", "esopranos", "thetudors",
+            "thevampirediaries", "thewire", "torchwood", "traveler", "trucalling", "trueblood", "uglybetty",
+            "veromcamars", "weeds", "whitechapel", "womensmurderclub", "xfiles"]
+
+# Fonction pour extraire le nom de la série à partir du nom du fichier
+def extract_series_name(filename):
+    for serie in nomSerie:
+        if serie.lower() in filename.lower():
+            return serie
+    return None
 
 # Fonction pour extraire le numéro de saison et d'épisode à partir du nom du fichier
 def extract_season_episode(filename):
@@ -32,7 +57,6 @@ def extract_season_episode(filename):
                 return int(numbers[0]), int(numbers[1])
     return None, None
 
-
 # Fonction pour extraire la langue à partir du nom du fichier
 def extract_language(filename):
     if re.search(r'\bVF\b', filename, re.IGNORECASE):
@@ -41,15 +65,16 @@ def extract_language(filename):
         return "English"
     return "English"
 
-
 # Fonction principale pour structurer les données en JSON
 def structure_data_to_json(root_path):
     data = []
 
     for root, _, files in os.walk(root_path):
-        series_name = os.path.basename(root)
         for file in files:
             if file.endswith(('.srt', '.sub', '.txt')):
+                series_name = extract_series_name(root)
+                if series_name is None:
+                    continue
                 season, episode = extract_season_episode(file)
                 language = extract_language(file)
 
@@ -77,5 +102,5 @@ def structure_data_to_json(root_path):
 
 # Exécution de la fonction principale
 if __name__ == "__main__":
-    root_path = os.path.join(os.getcwd(),'sous-titres')
+    root_path = os.path.join(os.getcwd(), 'sous-titres')
     structure_data_to_json(root_path)
