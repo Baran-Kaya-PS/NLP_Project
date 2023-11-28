34afdc6e66e696fd3d0f2e3b5a6a82ab14116e9f
Théo <89530648+SpottyShadow@users.noreply.github.com>
2023-11-28 14:12:36 +0100

    Version finale

--- Cleaner.py
+++ Cleaner.py
@@ -1,13 +1,7 @@
 import os
 import zipfile
 import re
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize, sent_tokenize
 from unidecode import unidecode
-
-
 
 def unzip_files(path):
     """
@@ -25,23 +19,7 @@ def unzip_files(path):
                     print(f"Skipped {file_path} as it's not a valid zip file.")
                 except Exception as e:
                     print(f"Error processing {file_path}: {e}")
-
-def process_files(path):
-    """
-    Processes all .srt and .sub files in the given directory and its subdirectories.
-    """
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if file.endswith('.srt') or file.endswith('.sub'):
-                file_path = os.path.join(root, file)
-                # Here, you can add any additional processing steps if needed.
-                print(f"Processed {file_path}")
-
-
-lemmatizer = WordNetLemmatizer()
-stop_words = set(stopwords.words(['english', 'french']))
-
-
+                    
 def clean_subtitles(file_path):
     """
     Cleans the subtitles in the given file by removing non-alphanumeric characters,
@@ -61,22 +39,14 @@ def clean_subtitles(file_path):
     content = re.sub(r'^\d+\s*$', '', content)  # Removes lines containing only digits
     content = re.sub(r'{\d+}{\d+}', '', content)  # Removes lines containing {digits}{digits}
 
-    # Tokenize, remove stopwords, and lemmatize
-    words = word_tokenize(content)
-    words = [lemmatizer.lemmatize(word) for word in words if word.lower() not in stop_words]
-
-    # Join the cleaned words back into a string
-    cleaned_content = ' '.join(words)
-
     # Overwrite the original file with the cleaned content
     try:
         with open(file_path, 'w', encoding='utf-8') as file:
-            file.write(cleaned_content)
+            file.write(content)
     except PermissionError:
         print(f"Skipping {file_path} due to a permission error.")
         return
-
-
+    
 def process_files(path):
     """
     Processes all .srt and .sub files in the given directory and its subdirectories.
@@ -87,23 +57,6 @@ def process_files(path):
                 file_path = os.path.join(root, file)
                 clean_subtitles(file_path)
                 print(f"Processed {file_path}")
-                
-
-def unzip_single_file(path, filename):
-    """
-    Unzips a single file located at the given path.
-    """
-    file_path = os.path.join(path, filename)
-    if file_path.endswith('.zip'):
-        try:
-            with zipfile.ZipFile(file_path, 'r') as zip_ref:
-                zip_ref.extractall(path)
-            print(f"Extracted {file_path}")
-        except zipfile.BadZipFile:
-            print(f"Skipped {file_path} as it's not a valid zip file.")
-        except Exception as e:
-            print(f"Error processing {file_path}: {e}")
-
 
 def removeNumberFromFiles(path):
     """
@@ -154,22 +107,6 @@ def remove_numbers_from_files(path):
 
     print(f"Finished processing {processed_files} out of {total_files} files.")
 
-
-def tokenize_text(text):
-    # Sentence tokenization
-    sentences = sent_tokenize(text)
-
-    # Word tokenization, removing punctuation, and lowercasing
-    tokenized_sentences = [word_tokenize(sentence) for sentence in sentences]
-    tokenized_sentences = [[word.lower() for word in sentence if word.isalpha()] for sentence in tokenized_sentences]
-
-    # Removing stop words (optional)
-    stop_words = set(stopwords.words('english'))  # Change 'english' to your desired language if needed
-    tokenized_sentences = [[word for word in sentence if word not in stop_words] for sentence in tokenized_sentences]
-
-    return tokenized_sentences
-
-
 def remove_accents_from_files(path):
     """
     Removes accents from all .srt, .sub, and .txt files in the given directory and its subdirectories.
@@ -179,29 +116,38 @@ def remove_accents_from_files(path):
             file_ext = os.path.splitext(file)[1]
             if file_ext in ['.srt', '.sub', '.txt']:
                 file_path = os.path.join(root, file)
-                with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
-                    content = f.read()
+                
+                try:
+                    with open(file_path, 'r') as f:
+                        content = f.read()
+                except UnicodeDecodeError:
+                    # Retry opening the file with UTF-8 encoding if an exception occurs
+                    with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
+                        content = f.read()
 
                 content_without_accents = unidecode(content)
 
                 with open(file_path, 'w', encoding='utf-8') as f:
                     f.write(content_without_accents)
-                print(f"Removed accents from {file_path}")
-                
+                print(f"Removed accents from {file_path}") 
                 
 def main():                
     Path = os.path.join(os.getcwd(), 'sous-titres')
 
     # Unzip all files in the directory
     unzip_files(Path)
-
+    
+    # Remove accents from all files in the directory
+    remove_accents_from_files(Path)
+    
     # Process all .srt and .sub files in the directory
     process_files(Path)
 
     # Clean all subtitles in the directory
     clean_subtitles(Path)
-
+    
     # Remove numbers from all files in the directory
     remove_numbers_from_files(Path)
+
 if __name__ == '__main__':
     main()
