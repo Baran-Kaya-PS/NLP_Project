aab35af14ef5c9f65c49470c0b474a75f3a1cc91
Théo <89530648+SpottyShadow@users.noreply.github.com>
2023-11-28 14:14:19 +0100

    Version finale

--- tokenizer.py
+++ tokenizer.py
@@ -1,6 +1,6 @@
 import os
 import json
-import nltk
+from collections import defaultdict
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
@@ -29,6 +29,47 @@ def tokenize_text_with_stopwords(text, language="english"):
     return tokens
 
 
+class Nettoyeur:
+
+    def __init__(self, data_path, stopwords_fr_path, stopwords_en_path):
+        with open(data_path, 'r', encoding='utf-8') as file:
+            self.data = json.load(file)
+
+        with open(stopwords_fr_path, 'r', encoding='utf-8') as file:
+            self.stopwords_fr = set(json.load(file))
+
+        with open(stopwords_en_path, 'r', encoding='utf-8') as file:
+            self.stopwords_en = set(json.load(file))
+
+    def clean_data(self):
+        token_frequency = defaultdict(int)
+        total_series = len(self.data)
+
+        # Calculer la fréquence de chaque token
+        for series in self.data:
+            for token in series["tokenized_content"]:
+                token_frequency[token] += 1
+
+        # Supprimer les tokens en fonction de la fréquence et des stopwords
+        for series in self.data:
+            content = series["tokenized_content"]
+            language = series["language"].lower()
+            stopwords = self.stopwords_fr if language == "french" else self.stopwords_en
+
+            series["tokenized_content"] = [
+                token for token in content
+                if token not in stopwords
+                and token_frequency[token] < 0.85 * total_series
+                and token_frequency[token] > 5  # Supprimer les tokens qui apparaissent moins de 5 fois
+            ]
+
+        # Mise à jour du fichier
+        with open(os.path.join(os.getcwd(), 'tokenized_data_cleaned.json'), 'w', encoding='utf-8') as file:
+            json.dump(self.data, file, ensure_ascii=False, indent=4)
+
+        print("Nettoyage terminé.")
+
+
 def main():
     # Define the path to the JSON data
     json_path = os.path.join(os.getcwd(), 'structured_data.json')
@@ -36,7 +77,6 @@ def main():
     # Load the JSON data
     with open(json_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
-
     # Tokenize the content for each series
     for series in data:
         language = series.get("language", "english")
@@ -54,6 +94,14 @@ def main():
         json.dump(data, file, ensure_ascii=False, indent=4)
 
     print("Tokenization completed and saved to tokenized_data.json")
+
+    # Clean the tokenized data
+    cleaner = Nettoyeur(
+        data_path=os.path.join(os.getcwd(), 'tokenized_data.json'),
+        stopwords_fr_path=os.path.join(os.getcwd(), 'stop_words_french.json'),
+        stopwords_en_path=os.path.join(os.getcwd(), 'stop_words_english.json')
+    )
+    cleaner.clean_data()
 
 
 if __name__ == '__main__':
