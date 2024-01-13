30824955fdd26ab84b0cbe59a863f69980ada7f8
Théo <89530648+SpottyShadow@users.noreply.github.com>
2024-01-13 15:39:43 +0100

    Update matrix_gen_obj.py

--- matrix_gen_obj.py
+++ matrix_gen_obj.py
@@ -5,14 +5,16 @@ from translate import Translator
 
 class MatrixGenerator:
     def __init__(self, data_path, output_path):
-        self.data_path = data_path
-        self.output_path = output_path
+        self.data_path = data_path  # Chemin d'accès aux données tokenisées
+        self.output_path = output_path  # Chemin de sortie pour la matrice TF-IDF
 
     def load_data(self):
+        # Charge les données tokenisées
         with open(self.data_path, 'r', encoding='utf-8') as file:
             return json.load(file)
 
     def generate_series_tokenized_content(self, data):
+        # tokenisation des données par série
         series_tokenized_content = {}
         for episode in data:
             series_name = episode['series_name']
@@ -24,12 +26,14 @@ class MatrixGenerator:
         return series_tokenized_content
 
     def generate_tfidf_matrix(self, series_tokenized_content):
+        # Crée une matrice TF-IDF à partir du contenu tokenisé
         tfidf_vectorizer = TfidfVectorizer()
         tfidf_matrix = tfidf_vectorizer.fit_transform([" ".join(episodes) for series, episodes in series_tokenized_content.items()])
         feature_names = tfidf_vectorizer.get_feature_names_out()
         return tfidf_matrix, feature_names
 
     def create_tfidf_json(self, series_tokenized_content, tfidf_matrix, feature_names):
+        # Crée un Json à partir de la matrice TF-IDF
         tfidf_matrix_json = {}
         for i, series_name in enumerate(series_tokenized_content.keys()):
             tfidf_matrix_json[series_name] = {}
@@ -41,10 +45,12 @@ class MatrixGenerator:
         return tfidf_matrix_json
 
     def save_json(self, tfidf_matrix_json):
+        # Sauvegarde le Json de la matrice TF-IDF
         with open(self.output_path, 'w', encoding='utf-8') as json_file:
             json.dump(tfidf_matrix_json, json_file, ensure_ascii=False, indent=4)
 
     def execute(self):
+        # Exécute toutes les étapes pour générer et sauvegarder la matrice TF-IDF
         data = self.load_data()
         series_tokenized_content = self.generate_series_tokenized_content(data)
         tfidf_matrix, feature_names = self.generate_tfidf_matrix(series_tokenized_content)
@@ -52,7 +58,7 @@ class MatrixGenerator:
         self.save_json(tfidf_matrix_json)
 
 if __name__ == '__main__':
-    data_path = 'tokenized_data_cleaned.json'  # Change according to the file path
+    data_path = 'tokenized_data_cleaned.json'  # Ajuster selon le chemin du fichier
     output_path = 'tfidf_matrix.json'
     matrix_generator = MatrixGenerator(data_path, output_path)
     matrix_generator.execute()
